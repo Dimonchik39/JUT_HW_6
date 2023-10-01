5e49d725f0d02cd5e5d4e1967b8dd3fbbf045e04
Dimonchik39 <112213539+Dimonchik39@users.noreply.github.com>
2023-10-01 18:48:48 +0200

    save_2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 import static org.example.AverageNumberList.getAverage;
 
 public class Main {
-    public static void main() {
+    public static void main(String[] args) {
         List<Integer> list = Arrays.asList(2, 43, 66, 25, 4, 67);
         List<Integer> list2 = Arrays.asList(2, 43, 6, 25, 4, 67);
 
